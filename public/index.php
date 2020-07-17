8ee64ac7b196030f61761f7c582fb7fe303a5997
Geoff Lambert <admin@vulpineinteractive.com>
2020-07-17 14:24:32 -0600

    home page markup done and some base sass added

--- public/index.php
+++ public/index.php
@@ -1,5 +1,108 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valerie K Lambert, LCSW CT</title>
+    <link href="css/styles.css" rel="stylesheet">
+</head>
 
-?>
+<body>
+    <div class="container">
+        <div class="navigation">
+            <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+    
+            <label for="navi-toggle" class="navigation__button">
+                <span class="navigation__icon">&nbsp;</span>
+            </label>
+    
+            <div class="navigation__background">&nbsp;</div>
+    
+            <nav class="navigation__nav">
+                <ul class="navigation__list">
+                    <li class="navigation__item"><a href="#" class="navigation__link">About Me</a></li>
+                    <li class="navigation__item"><a href="#" class="navigation__link">Office Policies</a></li>
+                    <li class="navigation__item"><a href="#" class="navigation__link">Resources</a></li>
+                </ul>
+            </nav>
+
+            <div class="navbar">
+                <div class="navbar__brand">
+                    <h1 class="nabar__brand--top">Valerie K Lambert</h1>
+                    <h1 class="nabar__brand--bottom">LCSW, CT</h1>
+                </div>
+                <ul class="navbar__list">
+                    <li class="navbar__item"><a href="#" class="navbar__link">About Me</a></li>
+                    <li class="navbar__item"><a href="#" class="navbar__link">Office Policies</a></li>
+                    <li class="navbar__item"><a href="#" class="navbar__link">Resources</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <header class="header">
+            <div class="header__hero-image">&nbsp;</div>
+            <div class="header__textarea">
+                <h2 class="header__text">Grief?</h2>
+                <h2 class="header__text">Chronic Illness?</h2>
+                <h2 class="header__text">Depression and Anxiety?</h2>
+                <h2 class="header__text">Hope</h2>
+            </div>
+        </header>
+
+        <main>
+            <section class="section-content-body">
+                <div class="card">
+                    <div class="card__header">
+                        <h3 class="card__header-text">You Are Not Alone</h3>
+                    </div>
+                    <div class="card__body">
+                        <p class="card__body-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-contact">
+                <div class="contact-box">
+                    <h3 class="contact-box__header">Let's Talk</h3>
+                    <a href="about" class="contact-box__thumbnail">
+                        <img src="https://dummyimage.com/200x200/000/fff" alt="Valerie Kay Lambert portrait" class="contact-box__thumbnail-image">
+                    </a>
+                    <p class="contact-box__address">1060 East 100 South #202</p>
+                    <p class="contact-box__address">Salt Lake City, Utah 84102</p>
+                    <div class="contact-box__contact-form">
+                        <a href="tel:801-533-5632" class="contact-box__button">
+                            <i class="contact-box__icon contact-box__icon--phone">&nbsp;</i>
+                            <span class="contact-box__text">801-533-5632</span>
+                        </a>
+                        <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
+                            <i class="contact-box__icon contact-box__icon--email">&nbsp;</i>
+                            <span class="contact-box__text">vkllcsw@gmail.com</span>
+                        </a>
+                        <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
+                            <i class="contact-box__icon contact-box__icon--secure">&nbsp;</i>
+                            <span class="contact-box__text">Request Secure Email</span>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer class="footer">
+            <p class="footer__copyright">
+                Copyright &copy; by Vulpine Interactive
+                Crying Photo by Tom Pumford on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+                Illness Photo by HH E on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+                Woman Crying Photo by Kat J on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+                Geese Photo by Gary Bendig on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+                Facepalm Photo by Adrian Swancar on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+            </p>
+        </footer>
+    </div>
+</body>
+
+</html>
