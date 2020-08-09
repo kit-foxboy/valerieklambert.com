fca44c7db9ab2e10837cd517c4944298f92031ff
Geoff Lambert <admin@vulpineinteractive.com>
2020-08-08 18:51:06 -0600

    updated homepage grid

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@
     <link href="css/styles.css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="container">
+<body class="container">
+    <header class="header">
         <div class="navigation">
             <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
     
@@ -23,118 +23,117 @@
                 <ul class="navigation__list">
                     <li class="navigation__item"><a href="#" class="navigation__link">About Me</a></li>
                     <li class="navigation__item"><a href="#" class="navigation__link">Office Policies</a></li>
+                    <li class="navigation__item"><a href="#" class="navigation__link">Forms</a></li>
                     <li class="navigation__item"><a href="#" class="navigation__link">Resources</a></li>
                 </ul>
             </nav>
-
-            <div class="navbar">
-                <div class="navbar__brand">
-                    <h1 class="nabar__brand--top">Valerie K Lambert</h1>
-                    <h1 class="nabar__brand--bottom">LCSW, CT</h1>
-                </div>
-                <ul class="navbar__list">
-                    <li class="navbar__item"><a href="#" class="navbar__link">About Me</a></li>
-                    <li class="navbar__item"><a href="#" class="navbar__link">Office Policies</a></li>
-                    <li class="navbar__item"><a href="#" class="navbar__link">Resources</a></li>
-                </ul>
+        </div>
+        <div class="navbar">
+            <div class="navbar__brand">
+                <h1 class="navbar__brand--top">Valerie K Lambert</h1>
+                <h1 class="navbar__brand--bottom">LCSW, CT</h1>
             </div>
+            <ul class="navbar__page-list">
+                <li class="navbar__page-item"><a href="#" class="navbar__link">About Me</a></li>
+                <li class="navbar__page-item"><a href="#" class="navbar__link">Office Policies</a></li>
+                <li class="navbar__page-item"><a href="#" class="navbar__link">Forms</a></li>
+                <li class="navbar__page-item"><a href="#" class="navbar__link">Resources</a></li>
+            </ul>
         </div>
 
-        <header class="header">
-            <div class="header__hero-image">&nbsp;</div>
-            <div class="header__textarea">
-                <h2 class="header__text">Grief?</h2>
-                <h2 class="header__text">Chronic Illness?</h2>
-                <h2 class="header__text">Depression and Anxiety?</h2>
-                <h2 class="header__text">Hope</h2>
+        <div class="header__hero-image">&nbsp;</div>
+        <div class="header__textarea">
+            <h2 class="header__text">Grief?</h2>
+            <h2 class="header__text">Chronic Illness?</h2>
+            <h2 class="header__text">Depression and Anxiety?</h2>
+            <h2 class="header__text">Hope</h2>
+        </div>
+    </header>
+
+    <main class="content home">
+        <section class="section-content-body">
+            <div class="card">
+                <div class="card__header">
+                    <h3 class="card__header-text">You Are Not Alone</h3>
+                </div>
+                <div class="card__body">
+                    <p class="card__body-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
+                    </p>
+                </div>
             </div>
-        </header>
+        </section>
 
-        <main>
-            <section class="section-content-body">
-                <div class="card">
-                    <div class="card__header">
-                        <h3 class="card__header-text">You Are Not Alone</h3>
-                    </div>
-                    <div class="card__body">
-                        <p class="card__body-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
-                        </p>
-                    </div>
-                </div>
-            </section>
-
-            <section class="section-contact">
-                <div class="contact-box">
-                    <h3 class="contact-box__header">Let's Talk</h3>
-                    <a href="about" class="contact-box__thumbnail">
-                        <img src="https://dummyimage.com/200x200/000/fff" alt="Valerie Kay Lambert portrait" class="contact-box__thumbnail-image">
+        <section class="section-contact">
+            <div class="contact-box">
+                <h3 class="contact-box__header">Let's Talk</h3>
+                <a href="about" class="contact-box__thumbnail">
+                    <img src="https://dummyimage.com/200x200/000/fff" alt="Valerie Kay Lambert portrait" class="contact-box__thumbnail-image">
+                </a>
+                <p class="contact-box__address">1060 East 100 South #202</p>
+                <p class="contact-box__address">Salt Lake City, Utah 84102</p>
+                <div class="contact-box__contact-form">
+                    <a href="tel:801-533-5632" class="contact-box__button">
+                        <i class="contact-box__icon contact-box__icon--phone">&nbsp;</i>
+                        <span class="contact-box__text">801-533-5632</span>
                     </a>
-                    <p class="contact-box__address">1060 East 100 South #202</p>
-                    <p class="contact-box__address">Salt Lake City, Utah 84102</p>
-                    <div class="contact-box__contact-form">
-                        <a href="tel:801-533-5632" class="contact-box__button">
-                            <i class="contact-box__icon contact-box__icon--phone">&nbsp;</i>
-                            <span class="contact-box__text">801-533-5632</span>
-                        </a>
-                        <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
-                            <i class="contact-box__icon contact-box__icon--email">&nbsp;</i>
-                            <span class="contact-box__text">vkllcsw@gmail.com</span>
-                        </a>
-                        <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
-                            <i class="contact-box__icon contact-box__icon--secure">&nbsp;</i>
-                            <span class="contact-box__text">Request Secure Email</span>
-                        </a>
-                    </div>
+                    <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
+                        <i class="contact-box__icon contact-box__icon--email">&nbsp;</i>
+                        <span class="contact-box__text">vkllcsw@gmail.com</span>
+                    </a>
+                    <a href="mailto:vkllcsw@gmail.com" class="contact-box__button">
+                        <i class="contact-box__icon contact-box__icon--secure">&nbsp;</i>
+                        <span class="contact-box__text">Request Secure Email</span>
+                    </a>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="section-features">
-                <div class="features">
-                    <h3 class="features__header">How I Can Help</h3>
-                    <div class="features__body">
-                        <div class="features__feature-box">
-                            <div class="features__icon-area">
-                                <i class="features__icon">&nbsp;</i>
-                                <h3 class="features__header">Telehealth</h3>
-                                <p class="features__text">Safe and secure online appointments</p>
-                                <a href="#" class="features__link">Learn More</a>
-                            </div>
+        <section class="section-features">
+            <div class="features">
+                <h3 class="features__header">How I Can Help</h3>
+                <div class="features__body">
+                    <div class="features__feature-box">
+                        <div class="features__icon-area">
+                            <i class="features__icon">&nbsp;</i>
+                            <h3 class="features__header">Telehealth</h3>
+                            <p class="features__text">Safe and secure online appointments</p>
+                            <a href="#" class="features__link">Learn More</a>
                         </div>
-                        <div class="features__feature-box">
-                            <div class="features__icon-area">
-                                <i class="features__icon">&nbsp;</i>
-                                <h3 class="features__header">Accepts Insurance</h3>
-                                <p class="features__text">Several common providers accepted</p>
-                                <a href="#" class="features__link">Learn More</a>
-                            </div>
+                    </div>
+                    <div class="features__feature-box">
+                        <div class="features__icon-area">
+                            <i class="features__icon">&nbsp;</i>
+                            <h3 class="features__header">Accepts Insurance</h3>
+                            <p class="features__text">Several common providers accepted</p>
+                            <a href="#" class="features__link">Learn More</a>
                         </div>
-                        <div class="features__feature-box">
-                            <div class="features__icon-area">
-                                <i class="features__icon">&nbsp;</i>
-                                <h3 class="features__header">Resources</h3>
-                                <p class="features__text">Free articles, advice, and inspirational quotes</p>
-                                <a href="#" class="features__link">Learn More</a>
-                            </div>
+                    </div>
+                    <div class="features__feature-box">
+                        <div class="features__icon-area">
+                            <i class="features__icon">&nbsp;</i>
+                            <h3 class="features__header">Resources</h3>
+                            <p class="features__text">Free articles, advice, and inspirational quotes</p>
+                            <a href="#" class="features__link">Learn More</a>
                         </div>
                     </div>
                 </div>
-            </section>
-        </main>
+            </div>
+        </section>
+    </main>
 
-        <footer class="footer">
-            <p class="footer__copyright">
-                Copyright &copy; by Vulpine Interactive
-                Crying Photo by Tom Pumford on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
-                Illness Photo by HH E on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
-                Woman Crying Photo by Kat J on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
-                Geese Photo by Gary Bendig on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
-                Facepalm Photo by Adrian Swancar on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
-            </p>
-        </footer>
-    </div>
+    <footer class="footer">
+        <p class="footer__copyright">
+            Copyright &copy; by Vulpine Interactive
+            Crying Photo by Tom Pumford on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+            Illness Photo by HH E on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+            Woman Crying Photo by Kat J on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+            Geese Photo by Gary Bendig on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+            Facepalm Photo by Adrian Swancar on <a href="http://www.unsplash.com" class="footer__link">Unsplash</a>
+        </p>
+    </footer>
 </body>
 
 </html>
