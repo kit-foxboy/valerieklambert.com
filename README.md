082918862d9766c288e4b98819d42b954a893e9b
Geoff Lambert <geoffl@vulpineinteractive.com>
2021-11-13 21:56:22 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # valerieklambert.com
 An advertising webpage for a therapy business
+
+## Pages
+- Home: Landing page with therapist mini-bio, best features, and contact information form
+- About Me: A mini-biography and description of professional qualifications
+- Office Policies: A basic overview of office policies, telehealth, and privacy
+- Forms: Downloadable paperwork needed to begin therapy
---
+++ public/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles.css" />
+    <title>Valerie K Lambert, LCSW, CT</title>
+</head>
+<body class="container">
+    <nav>
+        <header class="header">
+            <div class="nav-container">
+                <div class="nav-background">
+                    &nbsp;
+                </div>
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Office Policies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Forms</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a href="#" class="nav-link">Helpful Resources</a>
+                        </li> -->
+                    </ul>
+                </nav>
+            </div>
+            <div class="navbar">
+                <h1 class="brand-header">Valerie K Lambert</h1>
+            </div>
+        </header>
+        <main class="intro">
+            <section class="intro-body">
+                <div class="text-block">
+                    <div class="text-block-header">
+                        <h3>You are not alone</h3>
+                    </div>
+                    <div class="text-block-body">
+                        <p class="text-body-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="contact">
+                <div class="contact-info">
+                    <div class="contact-info-header">
+                        <h4>Let's Talk</h4>
+                    </div>
+                    <div class="contact-link-container">
+                        <a href="tel:801-533-5632" class="contact-link">
+                            <i class="phone-icon">&nbsp;</i>
+                            <span class="contact-link-text mobile">Call Me</span>
+                            <span class="contact-link-text">801-533-5632</span>
+                        </a>
+                        <a href="mailto:vkllcsw@gmail.com" class="contact-link">
+                            <i class="email-icon">&nbsp;</i>
+                            <span class="contact-link-text mobile">Email Me</span>
+                            <span class="contact-link-text">vkllcsw@gmail.com</span>
+                        </a>
+                        <div class="contact-button">
+                            <i class="secure-icon">&nbsp;</i>
+                            <span class="contact-link-text">Request Secure Email</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="features">
+                <div class="features">
+                    <h3 class="features-header">How I Can Help</h3>
+                    <div class="feature-box-container">
+                        <div class="feature-box">
+                            <i class="telehealth-icon">&nbsp;</i>
+                            <h3 class="feature-header">Telehealth</h3>
+                            <p class="feature-text">Safe and secure online appointments</p>
+                            <a href="#" class="feature-link">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="feature-box-container">
+                        <div class="feature-box">
+                            <i class="insurance-icon">&nbsp;</i>
+                            <h3 class="feature-header">Accepts Most Insurance</h3>
+                            <p class="feature-text">Many common providers accepted</p>
+                            <a href="#" class="feature-link">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="feature-box-container">
+                        <div class="feature-box">
+                            <i class="insurance-icon">&nbsp;</i>
+                            <h3 class="feature-header">Resources</h3>
+                            <p class="feature-text">Free articles, advice, and inspirational quotes</p>
+                            <a href="#" class="feature-link">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="feature-details">
+                <!-- TODO -->
+            </section>
+        </main>
+        <footer>
+            <p class="copyright">
+                Copyright &copy; by Vulpine Interactive
+            </p>
+        </footer>
+    </nav>
+</body>
+</html>
