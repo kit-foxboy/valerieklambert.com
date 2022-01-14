dea21072df022d5275c42e77171fa37cfa4e7b0e
kit-foxboy <admin@vulpineinteractive.com>
2022-01-14 00:02:39 -0700

    mobile header setup, nav and animations incomplete

--- public/index.php
+++ public/index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/normalize.css" />
     <link rel="stylesheet" href="/css/styles.css" />
     <title>Valerie K Lambert, LCSW, CT</title>
 </head>
 <body class="container">
-    <nav>
-        <header class="header">
+    <header class="header">
+        <nav>
             <div class="nav-container">
                 <div class="nav-background">
                     &nbsp;
@@ -25,92 +26,103 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link">Forms</a>
                         </li>
-                        <!-- <li class="nav-item">
+                        <li class="nav-item">
                             <a href="#" class="nav-link">Helpful Resources</a>
-                        </li> -->
+                        </li>
                     </ul>
                 </nav>
             </div>
             <div class="navbar">
                 <h1 class="brand-header">Valerie K Lambert</h1>
+                <button type="button" class="nav-button">NAV</button>
             </div>
-        </header>
-        <main class="intro">
-            <section class="intro-body">
-                <div class="text-block">
-                    <div class="text-block-header">
-                        <h3>You are not alone</h3>
-                    </div>
-                    <div class="text-block-body">
-                        <p class="text-body-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
-                        </p>
+        </nav>
+        <div class="hero-banner">
+            <!-- <img class="hero-image" src="/images/grief.jpg" /> -->
+            <div class="hero-text-container">
+                <h3 class="hero-text">Grief?</h3>
+                <h3 class="hero-text">Chronic Illness?</h3>
+                <h3 class="hero-text">Axiety?</h3>
+            </div>
+        </div>
+    </header>
+    <main class="intro">
+        <section class="intro-body">
+            <div class="text-block">
+                <div class="text-block-header">
+                    <h3>You are not alone</h3>
+                </div>
+                <div class="text-block-body">
+                    <p class="text-body-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto minima modi quos esse facere animi accusantium quo et aut mollitia dolorum pariatur doloremque, ullam maiores ipsam. Est non obcaecati esse?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quam officia minus ratione autem. Accusantium sit sint autem facilis voluptatibus veritatis vero nulla aspernatur exercitationem consectetur, corrupti impedit vitae dicta!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam placeat aliquid veniam quas hic. Quo odit nostrum nesciunt sint sed, repudiandae laboriosam esse, magnam quis necessitatibus incidunt eaque. Ullam, a!
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="contact">
+            <div class="contact-info">
+                <div class="contact-info-header">
+                    <h4>Let's Talk</h4>
+                </div>
+                <div class="contact-link-container">
+                    <a href="tel:801-533-5632" class="contact-link">
+                        <i class="phone-icon">&nbsp;</i>
+                        <span class="contact-link-text mobile">Call Me</span>
+                        <span class="contact-link-text">801-533-5632</span>
+                    </a>
+                    <a href="mailto:vkllcsw@gmail.com" class="contact-link">
+                        <i class="email-icon">&nbsp;</i>
+                        <span class="contact-link-text mobile">Email Me</span>
+                        <span class="contact-link-text">vkllcsw@gmail.com</span>
+                    </a>
+                    <div class="contact-button">
+                        <i class="secure-icon">&nbsp;</i>
+                        <span class="contact-link-text">Request Secure Email</span>
                     </div>
                 </div>
-            </section>
-            <section class="contact">
-                <div class="contact-info">
-                    <div class="contact-info-header">
-                        <h4>Let's Talk</h4>
-                    </div>
-                    <div class="contact-link-container">
-                        <a href="tel:801-533-5632" class="contact-link">
-                            <i class="phone-icon">&nbsp;</i>
-                            <span class="contact-link-text mobile">Call Me</span>
-                            <span class="contact-link-text">801-533-5632</span>
-                        </a>
-                        <a href="mailto:vkllcsw@gmail.com" class="contact-link">
-                            <i class="email-icon">&nbsp;</i>
-                            <span class="contact-link-text mobile">Email Me</span>
-                            <span class="contact-link-text">vkllcsw@gmail.com</span>
-                        </a>
-                        <div class="contact-button">
-                            <i class="secure-icon">&nbsp;</i>
-                            <span class="contact-link-text">Request Secure Email</span>
-                        </div>
+            </div>
+        </section>
+        <section class="features">
+            <div class="features">
+                <h3 class="features-header">How I Can Help</h3>
+                <div class="feature-box-container">
+                    <div class="feature-box">
+                        <i class="telehealth-icon">&nbsp;</i>
+                        <h3 class="feature-header">Telehealth</h3>
+                        <p class="feature-text">Safe and secure online appointments</p>
+                        <a href="#" class="feature-link">Learn More</a>
                     </div>
                 </div>
-            </section>
-            <section class="features">
-                <div class="features">
-                    <h3 class="features-header">How I Can Help</h3>
-                    <div class="feature-box-container">
-                        <div class="feature-box">
-                            <i class="telehealth-icon">&nbsp;</i>
-                            <h3 class="feature-header">Telehealth</h3>
-                            <p class="feature-text">Safe and secure online appointments</p>
-                            <a href="#" class="feature-link">Learn More</a>
-                        </div>
-                    </div>
-                    <div class="feature-box-container">
-                        <div class="feature-box">
-                            <i class="insurance-icon">&nbsp;</i>
-                            <h3 class="feature-header">Accepts Most Insurance</h3>
-                            <p class="feature-text">Many common providers accepted</p>
-                            <a href="#" class="feature-link">Learn More</a>
-                        </div>
-                    </div>
-                    <div class="feature-box-container">
-                        <div class="feature-box">
-                            <i class="insurance-icon">&nbsp;</i>
-                            <h3 class="feature-header">Resources</h3>
-                            <p class="feature-text">Free articles, advice, and inspirational quotes</p>
-                            <a href="#" class="feature-link">Learn More</a>
-                        </div>
+                <div class="feature-box-container">
+                    <div class="feature-box">
+                        <i class="insurance-icon">&nbsp;</i>
+                        <h3 class="feature-header">Accepts Most Insurance</h3>
+                        <p class="feature-text">Many common providers accepted</p>
+                        <a href="#" class="feature-link">Learn More</a>
                     </div>
                 </div>
-            </section>
-            <section class="feature-details">
-                <!-- TODO -->
-            </section>
-        </main>
-        <footer>
-            <p class="copyright">
-                Copyright &copy; by Vulpine Interactive
-            </p>
-        </footer>
-    </nav>
+                <div class="feature-box-container">
+                    <div class="feature-box">
+                        <i class="insurance-icon">&nbsp;</i>
+                        <h3 class="feature-header">Resources</h3>
+                        <p class="feature-text">Free articles, advice, and inspirational quotes</p>
+                        <a href="#" class="feature-link">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="feature-details">
+            <!-- TODO -->
+        </section>
+    </main>
+    <footer>
+        <p class="copyright">
+            Copyright &copy; by Vulpine Interactive
+        </p>
+        Photo by <a href="https://unsplash.com/@a_d_s_w?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Adrian Swancar</a> on <a href="https://unsplash.com/s/photos/anxious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </footer>
+    <script type="text/javascript" src="js/animation.js"></script>
 </body>
 </html>
